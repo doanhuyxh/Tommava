2d5e460eda57e7d02fe77863f167bdd0a95bb307
banggiahung <banggiahung131@gmail.com>
2023-10-19 12:23:57 +0700

    git change

---
+++ Models/EpisodesVideo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tommava.Models
+{
+    public class EpisodesVideo
+    {
+        [Key]
+        public int Id { get; set; }
+        public int VideoId { get; set; }
+        public int EpisodeNumber { get; set; }
+        public int ViewCount { get; set; }
+        public string TitleEN { get; set; }
+        public string TitleVN { get; set; }
+        public string Description { get; set; }
+        public string VideoLink { get; set; }
+        public string ImgLink { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}
